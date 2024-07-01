40a84b4c9442ddf10f4445ffd5dbc6ce399aee23
mbfuss <145613641+mbfuss@users.noreply.github.com>
2024-07-01 06:45:32 +0400

    fix docker connection, test, bd

---
+++ src/test/java/repository/UserRepositoryTest.java
@@ -0,0 +1,51 @@
+package repository;
+
+import org.example.model.User;
+import org.example.repository.UserRepository;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+import java.util.Properties;
+
+import static org.junit.Assert.*;
+
+public class UserRepositoryTest {
+    @ClassRule
+    public static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:latest")
+            .withDatabaseName("coworking")
+            .withUsername("coworking")
+            .withPassword("coworking");
+
+    private UserRepository userRepository;
+
+    @Before
+    public void setUp() {
+        Properties properties = new Properties();
+        properties.setProperty("db.url", postgreSQLContainer.getJdbcUrl());
+        properties.setProperty("db.username", postgreSQLContainer.getUsername());
+        properties.setProperty("db.password", postgreSQLContainer.getPassword());
+
+        userRepository = new UserRepository(
+                properties.getProperty("db.url"),
+                properties.getProperty("db.username"),
+                properties.getProperty("db.password")
+        );
+    }
+
+    @Test
+    public void testSaveAndFindUser() {
+        userRepository.deleteAll();
+
+        User user = new User();
+        user.setUsername("testuser");
+        user.setPassword("testpassword");
+        user.setAdmin(false);
+        userRepository.save(user);
+
+        User foundUser = userRepository.findByUsername("testuser");
+        assertNotNull(foundUser);
+        assertEquals("testuser", foundUser.getUsername());
+    }
+}
